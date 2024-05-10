fb51a3265e699b073f266adc7754adbb18b9b8dd
Manish Mittal <manishmittal19@yahoo.com>
2024-05-10 14:50:53 -0700

    remove keys

--- eks-setup/peering.tf
+++ eks-setup/peering.tf
@@ -1,5 +1,5 @@
 resource "aws_route" "bastion_to_eks" {
   route_table_id            = module.eks-vpc.private_route_table_ids[0]
   destination_cidr_block    = "10.1.0.0/16"
-  vpc_peering_connection_id = "pcx-0d007fdc3eeba51a9"
+  vpc_peering_connection_id = ""
 }
